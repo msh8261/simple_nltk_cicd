22d97f1ea9ad8bb08f568be9f033575a95d218b7
mohser sh <sh1361m@gmail.com>
2021-02-23 09:24:43 +0800

    fix

--- test_api.py
+++ test_api.py
@@ -2,19 +2,19 @@ import requests
 import json
 
 
-input_data =  {"body": ['The sky is blue and beautiful.',
-                  'Love this blue and beautiful sky!',
-                  'The quick brown fox jumps over the lazy dog.',
+input_data =  {"body": ["The sky is blue and beautiful.",
+                  "Love this blue and beautiful sky!",
+                  "The quick brown fox jumps over the lazy dog.",
                   "A king's breakfast has sausages, ham, bacon, eggs, toast and beans",
-                  'I love green eggs, ham, sausages and bacon!',
-                  'The brown fox is quick and the blue dog is lazy!',
-                  'The sky is very blue and the sky is very beautiful today',
-                  'The dog is lazy but the brown fox is quick!'    
+                  "I love green eggs, ham, sausages and bacon!",
+                  "The brown fox is quick and the blue dog is lazy!",
+                  "The sky is very blue and the sky is very beautiful today",
+                  "The dog is lazy but the brown fox is quick!"    
             ]
         }
 
 
-url = "https://y5umgtbrt0.execute-api.us-east-2.amazonaws.com/dev/nltk"
+url = " https://fv2bmksil3.execute-api.us-east-2.amazonaws.com/dev/nltk"
 
 r = requests.post(url, json=input_data)
 print("result without securely key: ", r.json())
